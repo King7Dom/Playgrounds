677bfa906144e78a7380f82cfb47b2013203df9f
Dominic Cheung <dom.with.a.inic+git@gmail.com>
2017-11-30 11:58:25 +1100

    Update POP playgorund

--- Protocol Oriented Programming.playground/Pages/Common Enum Decodable.xcplaygroundpage/Contents.swift
+++ Protocol Oriented Programming.playground/Pages/Common Enum Decodable.xcplaygroundpage/Contents.swift
@@ -1,7 +1,23 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var str = "Hello, playground"
+let fileURL = Bundle.main.url(forResource: "JSON", withExtension: "json")!
+let jsonData = try Data(contentsOf: fileURL)
 
-//: [Next](@next)
+let transactions = try JSONDecoder().decode([Transaction].self, from: jsonData)
+
+if case .payment(let payment) = transactions[0] {
+    payment.amount
+    payment.fromUser
+    payment.toUser
+}
+
+if case .request(let request) = transactions[1] {
+    request
+}
+
+if case .split(let split) = transactions[2] {
+    split.amount
+    split.users
+    split.referenceID
+}
--- Protocol Oriented Programming.playground/Pages/Common Enum Decodable.xcplaygroundpage/Sources/Transaction.swift
+++ Protocol Oriented Programming.playground/Pages/Common Enum Decodable.xcplaygroundpage/Sources/Transaction.swift
@@ -1,25 +1,52 @@
 import Foundation
 
-enum Transaction {
-    case payment
-    case request
-    case split
+public enum Transaction: Decodable {
+    case payment(Payment)
+    case request(Request)
+    case split(Split)
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+    }
+
+    private enum TransactionType: String, Decodable {
+        case payment = "payment"
+        case request = "request"
+        case split = "split"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(TransactionType.self, forKey: CodingKeys.type)
+
+        switch type {
+        case .payment: self = .payment(try Payment(from: decoder))
+        case .request: self = try .request(Request(from: decoder))
+        case .split: self = try .split(Split(from: decoder))
+        }
+    }
 }
 
-struct Payment {
-    let amount: Int
-    let fromUser: User
-    let toUser: User
+public struct Payment: Codable {
+    public let amount: Int
+    public let fromUser: User
+    public let toUser: User
 }
 
-struct Request {
-    let amount: Int
-    let fromUser: User
-    let toUser: User
-    let referenceID: UUID
+public struct Request: Codable {
+    public let amount: Int
+    public let fromUser: User
+    public let toUser: User
+    public let referenceID: UUID
 }
 
-struct User: Codable {
-    let name: String
-    let identifier: String
+public struct Split: Codable {
+    public let amount: Int
+    public let users: [User]
+    public let referenceID: UUID
+}
+
+public struct User: Codable {
+    public let name: String
+    public let identifier: String
 }
